b0855d886c99c8bc5acafc8bf38fab30763b5e1a
Nakib <nakkibcse@gmail.com>
2026-02-19 11:27:34 +0600

    Password hashing with salt practice

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context) : BaseApiController
+{
+   [HttpPost("register")] // api/account/register
+   public async Task<ActionResult<AppUsers>> Register(string email, string displayName, string password)
+    {
+        using var hmac = new HMACSHA512();
+        var user = new AppUsers
+        {
+            DisplayName = displayName,
+            Email = email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            PasswordSalt = hmac.Key    
+        };
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user;
+    } 
+}
