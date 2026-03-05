23992c62361a44ff448e6ee8410e63051ecaedb8
Nakib <nakkibcse@gmail.com>
2026-03-05 12:35:07 +0600

    Added angular form template for login

--- client/src/layout/nav/nav.component.ts
+++ client/src/layout/nav/nav.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  protected creds: any = {}
 
+  login(){
+    console.log(this.creds);
+  }
 }
