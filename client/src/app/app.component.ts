0a580f7e93559a3fd3b8a6aee8ce3cdc07171578
Nakib <nakkibcse@gmail.com>
2026-03-11 12:29:38 +0600

    Added route animation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,45 +1,12 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal, Signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
-import { errorContext } from 'rxjs/internal/util/errorContext';
 import { NavComponent } from "../layout/nav/nav.component";
-import { AccountServiceService } from '../Core/services/account-service.service';
-import { HomeComponent } from "../features/home/home.component";
-import { User } from '../types/user';
 import { Router, RouterOutlet } from '@angular/router';
-import { NgClass } from '@angular/common';
-
 @Component({
   selector: 'app-root',
   imports: [NavComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
-  private accountService = inject(AccountServiceService);
+export class AppComponent {
   protected router = inject(Router);
-  private http = inject(HttpClient);
-  protected title = 'Social App';
-  protected members = signal<User[]>([]);
-
-   async ngOnInit(){
-    this.members.set(await this.getMembers())
-    this.setCurrentUser();
-  }
-  setCurrentUser(){
-    const userString = localStorage.getItem('user');
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-  async getMembers(){
-    try {
-
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-      
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
 }
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -9,7 +9,7 @@ import { last, lastValueFrom } from 'rxjs';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideHttpClient(),
     provideAppInitializer(async () => {
       const initService = inject(InitServiceService);
