15858d1e2c20fbb12b8a6d1c8bd63ad105b2a364
Nakib <nakkibcse@gmail.com>
2026-04-21 16:56:43 +0600

    updated

--- API/Entities/AppUsers.cs
+++ API/Entities/AppUsers.cs
@@ -7,6 +7,11 @@ public class AppUsers
     public string ID { get; set; } = Guid.NewGuid().ToString();
     public required string DisplayName {get; set;}
     public required string Email {get; set;}
+
+    public string? ImageUrl { get; set; }
     public required byte[] PasswordHash {get; set;}
     public required byte[] PasswordSalt {get; set;}
+
+    //navigation propertie
+    public Member Member { get; set; } = null!;
 }
---
+++ API/Entities/Photo.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace API.Entities;
+
+public class Photo
+{
+    public int Id {get; set;}
+    public required string Url {get; set;}
+    public string? PublicId {get; set;}
+
+    //Navigation propertie
+    public Member Member { get; set; } = null!;
+
+}
